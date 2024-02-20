692f08c7fcc371f41544efb79b3d9adb843f8549
William Santana <williamandrade1058@gmail.com>
2024-02-20 14:32:11 -0300

    create screenmatch

---
+++ src/Filme.java
@@ -0,0 +1,21 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacao;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+    
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+}
