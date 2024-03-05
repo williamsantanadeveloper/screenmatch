c4e8f7296320162331981fadeb1d222429a084e8
William Santana <williamandrade1058@gmail.com>
2024-03-05 17:20:01 -0300

    refact

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,20 @@
+import br.com.alura.screenmath.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+
         System.out.println(meuFilme.pegaMedia());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
     }
 }
---
+++ src/br/com/alura/screenmath/modelos/Filme.java
@@ -0,0 +1,42 @@
+package br.com.alura.screenmath.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacao;
+    private int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacao() {
+        return totalDeAvaliacao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+    }
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+}
